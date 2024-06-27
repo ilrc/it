8def026f75cff74b5bd6c45b73d8240ad18546e9
ilrc <159825980+ilrc@users.noreply.github.com>
2024-06-27 22:28:18 +0300

    service worker

--- js/main.js
+++ js/main.js
@@ -107,3 +107,10 @@
     
 })(jQuery);
 
+
+//Service Worker
+import * as serviceWorker from './serviceWorker';
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+serviceWorker.register();
